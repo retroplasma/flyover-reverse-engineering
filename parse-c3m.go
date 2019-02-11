64e9bba7add281ab7567d4cbaaa95cb0675e863c
retroplasma <46618410+retroplasma@users.noreply.github.com>
2019-02-11 23:43:29 +0000

    Parse JPEG

--- parse-c3m.go
+++ parse-c3m.go
@@ -7,24 +7,32 @@ import (
 	"log"
 	"math"
 	"os"
+	"os/exec"
+	"time"
 )
 
 var l = log.New(os.Stderr, "", 0)
+var out = false
 
 func main() {
 
-	/*cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()*/
-
-	if len(os.Args) != 2 {
+	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: %s [c3m_file]\n", os.Args[0])
 		os.Exit(1)
+	}
+
+	if len(os.Args) == 3 && os.Args[2] == "out" {
+		out = true
 	}
 
 	file := os.Args[1]
 	data, err := ioutil.ReadFile(file)
 	checkPanic(err)
+
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	l.Printf("%v\n\n", time.Now())
 
 	l.Printf("File size: %d bytes\n", len(data))
 	if len(data) < 4 || data[0] != 'C' || data[1] != '3' || data[2] != 'M' {
@@ -52,6 +60,7 @@ func parseC3Mv3(data []byte) {
 	l.SetPrefix(l.Prefix() + "  ")
 
 	numberOfItems := int(data[5])
+	l.Printf("Number of items: %d\n", numberOfItems)
 	processedItems := 0
 	offset := 6
 
@@ -68,18 +77,20 @@ func parseC3Mv3(data []byte) {
 			l.SetPrefix(l.Prefix() + "  ")
 			parseMaterial(data, &offset)
 		case 2:
-			l.Printf("- Mesh at 0x%x (%d)", offset, offset)
+			l.Printf("Mesh at 0x%x (%d)", offset, offset)
 			l.SetPrefix(l.Prefix() + "  ")
-			panic("Type 2 not implemented")
+			l.Printf("Not implemented, can't skip yet")
+			os.Exit(1)
 		case 3:
 			l.Printf("Scene Graph? / Animation? at 0x%x (%d)", offset, offset)
 			l.SetPrefix(l.Prefix() + "  ")
-			panic("Type 3 not implemented")
+			l.Printf("Not implemented, can't skip yet")
+			os.Exit(1)
 		default:
 			panic("Invalid item type")
 		}
 
-		if processedItems >= numberOfItems {
+		if processedItems+1 >= numberOfItems {
 			l.Println("All items processed")
 			return
 		}
@@ -89,15 +100,15 @@ func parseC3Mv3(data []byte) {
 
 func parseHeader(data []byte, offset *int) {
 
-	qx := readDouble(data, *offset+9)
-	qy := readDouble(data, *offset+17)
-	qz := readDouble(data, *offset+25)
-	qw := readDouble(data, *offset+33)
+	qx := readFloat64(data, *offset+9)
+	qy := readFloat64(data, *offset+17)
+	qz := readFloat64(data, *offset+25)
+	qw := readFloat64(data, *offset+33)
 	l.Printf("Rotation Quaternion XYZW:     [% 16f,% 16f,% 15f,% f ]\n", qx, qy, qz, qw)
 
-	x := readDouble(data, *offset+41)
-	y := readDouble(data, *offset+49)
-	z := readDouble(data, *offset+57)
+	x := readFloat64(data, *offset+41)
+	y := readFloat64(data, *offset+49)
+	z := readFloat64(data, *offset+57)
 	l.Printf("Translation ECEF XYZ:         [% 16f,% 16f,% 15f ]\n", x, y, z)
 
 	m0 := 1 - 2*qy*qy - 2*qz*qz
@@ -119,14 +130,89 @@ func parseHeader(data []byte, offset *int) {
 
 func parseMaterial(data []byte, offset *int) {
 	*offset += 5
-	panic("Type 1 not implemented")
+	numberOfItems := int(readInt32(data, *offset+0))
+	l.Printf("Number of materials: %d \n", numberOfItems)
+	processedItems := 0
+	*offset += 4
+
+	pfx := l.Prefix()
+	l.SetPrefix(l.Prefix() + "  ")
+
+	for {
+		l.SetPrefix(pfx)
+		materialType := data[*offset]
+
+		switch materialType {
+		case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10:
+			l.Printf("- Material type %d", materialType)
+			l.SetPrefix(l.Prefix() + "  ")
+
+			textureFormat := data[*offset+3]
+			textureOffset := readInt32(data, *offset+4)
+			textureLength := readInt32(data, *offset+8)
+			textureLength2 := readInt32(data, *offset+12)
+
+			l.SetPrefix(l.Prefix() + "  ")
+			l.Printf("texOff: %d, texLen1: %d, texLen2: %d", textureOffset, textureLength, textureLength2)
+			switch textureFormat {
+			case 0:
+				l.Printf("Format: JPEG")
+
+				if out {
+					fn := fmt.Sprintf("/tmp/jpg/%d.jpg", processedItems)
+					err := ioutil.WriteFile(fn, data[textureOffset:textureOffset+textureLength2], 0655)
+					checkPanic(err)
+					l.Printf("Exported: %s", fn)
+				}
+
+				*offset += 16
+			default:
+				panic(fmt.Sprintf("Unsupported textureFormat %d", textureFormat))
+			}
+		default:
+			panic(fmt.Sprintf("materialType %d not implemented", materialType))
+		}
+
+		l.SetPrefix(pfx)
+		if processedItems+1 >= numberOfItems {
+			l.Println("All materials processed")
+			return
+		}
+		processedItems++
+	}
 }
 
-func readDouble(bytes []byte, offset int) float64 {
-	bits := binary.LittleEndian.Uint64(bytes[offset : offset+8])
-	float := math.Float64frombits(bits)
-	return float
+/*
+ * binary readers
+ */
+
+func readFloat32(data []byte, offset int) float32 {
+	return math.Float32frombits(binary.LittleEndian.Uint32(data[offset:]))
 }
+
+func readFloat64(data []byte, offset int) float64 {
+	return math.Float64frombits(binary.LittleEndian.Uint64(data[offset:]))
+}
+
+func readInt32(data []byte, offset int) int32 {
+	return int32(binary.LittleEndian.Uint32(data[offset:]))
+}
+
+func readUInt32(data []byte, offset int) uint32 {
+	return binary.LittleEndian.Uint32(data[offset:])
+}
+
+func readInt64(data []byte, offset int) int64 {
+	return int64(binary.LittleEndian.Uint64(data[offset:]))
+}
+
+func readUInt64(data []byte, offset int) uint64 {
+	return binary.LittleEndian.Uint64(data[offset:])
+}
+
+/*
+ * other
+ */
 
 func checkPanic(e error) {
 	if e != nil {

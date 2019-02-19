0a93bf82278f74aa047cbdb3d6ffcb928bb5b138
retroplasma <46618410+retroplasma@users.noreply.github.com>
2019-02-19 22:55:27 +0000

    fix size of matrix from quaternion

--- pkg/mth/mth.go
+++ pkg/mth/mth.go
@@ -1,7 +1,7 @@
 package mth
 
 // QuaternionToMatrix converts quaternion values to 3x3 matrix
-func QuaternionToMatrix(qx, qy, qz, qw float64) (m [10]float64) {
+func QuaternionToMatrix(qx, qy, qz, qw float64) (m [9]float64) {
 	m[0] = 1 - 2*qy*qy - 2*qz*qz
 	m[1] = 2*qx*qy - 2*qw*qz
 	m[2] = 2*qx*qz + 2*qw*qy

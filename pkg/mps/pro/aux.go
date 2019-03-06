6449d67c04bd0652f8558082a19d939e46856ec8
retroplasma <46618410+retroplasma@users.noreply.github.com>
2019-03-06 17:10:34 +0100

    Add auxiliary methods to resource manifest

---
+++ pkg/mps/pro/aux.go
@@ -0,0 +1,33 @@
+package pro
+
+import (
+	"errors"
+	fmt "fmt"
+	"regexp"
+)
+
+func (rm ResourceManifest) URLPrefixFromStyleID(styleID ResourceManifest_StyleConfig_StyleID) (pfx string, err error) {
+	for _, v := range rm.StyleConfig {
+		if v.StyleId == styleID {
+			pfx = v.UrlPrefix_1
+			break
+		}
+	}
+	if pfx == "" {
+		err = errors.New(fmt.Sprint("no url prefix found for style", styleID))
+	}
+	return
+}
+
+func (rm ResourceManifest) CacheFileNameFromRegexp(regexp *regexp.Regexp) (fn string, err error) {
+	for _, cf := range rm.CacheFile {
+		if regexp.MatchString(cf.FileName) {
+			fn = cf.FileName
+			break
+		}
+	}
+	if fn == "" {
+		err = errors.New(fmt.Sprint("no cache file for regexp", regexp.String()))
+	}
+	return
+}

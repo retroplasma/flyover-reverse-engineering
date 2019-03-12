e1bb4306ce55de288f65f2a84c6e69459a5e8b78
retroplasma <46618410+retroplasma@users.noreply.github.com>
2019-03-12 18:18:48 +0100

    Don’t allocate buffers in bin functions

--- pkg/bin/bin.go
+++ pkg/bin/bin.go
@@ -11,66 +11,32 @@ import (
 
 // WriteUInt64 writes a uint64 value to data at offset
 func WriteUInt64(data []byte, offset int, value uint64) {
-	buf := make([]byte, 8)
-	binary.LittleEndian.PutUint64(buf, value)
-	data[offset+0] = buf[0]
-	data[offset+1] = buf[1]
-	data[offset+2] = buf[2]
-	data[offset+3] = buf[3]
-	data[offset+4] = buf[4]
-	data[offset+5] = buf[5]
-	data[offset+6] = buf[6]
-	data[offset+7] = buf[7]
+	binary.LittleEndian.PutUint64(data[offset:], value)
 }
 
 // WriteInt64 writes an int64 value to data at offset
 func WriteInt64(data []byte, offset int, value int64) {
-	buf := make([]byte, 8)
-	binary.LittleEndian.PutUint64(buf, uint64(value))
-	data[offset+0] = buf[0]
-	data[offset+1] = buf[1]
-	data[offset+2] = buf[2]
-	data[offset+3] = buf[3]
-	data[offset+4] = buf[4]
-	data[offset+5] = buf[5]
-	data[offset+6] = buf[6]
-	data[offset+7] = buf[7]
+	binary.LittleEndian.PutUint64(data[offset:], uint64(value))
 }
 
 // WriteInt32 writes an int32 value to data at offset
 func WriteInt32(data []byte, offset int, value int32) {
-	buf := make([]byte, 4)
-	binary.LittleEndian.PutUint32(buf, uint32(value))
-	data[offset+0] = buf[0]
-	data[offset+1] = buf[1]
-	data[offset+2] = buf[2]
-	data[offset+3] = buf[3]
+	binary.LittleEndian.PutUint32(data[offset:], uint32(value))
 }
 
 // WriteUInt32 writes a uint32 value to data at offset
 func WriteUInt32(data []byte, offset int, value uint32) {
-	buf := make([]byte, 4)
-	binary.LittleEndian.PutUint32(buf, value)
-	data[offset+0] = buf[0]
-	data[offset+1] = buf[1]
-	data[offset+2] = buf[2]
-	data[offset+3] = buf[3]
+	binary.LittleEndian.PutUint32(data[offset:], value)
 }
 
 // WriteInt16 writes an int16 value to data at offset
 func WriteInt16(data []byte, offset int, value int16) {
-	buf := make([]byte, 2)
-	binary.LittleEndian.PutUint16(buf, uint16(value))
-	data[offset+0] = buf[0]
-	data[offset+1] = buf[1]
+	binary.LittleEndian.PutUint16(data[offset:], uint16(value))
 }
 
 // WriteUInt16 writes a uint16 value to data at offset
 func WriteUInt16(data []byte, offset int, value uint16) {
-	buf := make([]byte, 2)
-	binary.LittleEndian.PutUint16(buf, value)
-	data[offset+0] = buf[0]
-	data[offset+1] = buf[1]
+	binary.LittleEndian.PutUint16(data[offset:], value)
 }
 
 /*
@@ -139,28 +105,11 @@ func ReadUInt64(data []byte, offset int) uint64 {
 
 // ByteSwapUInt64 returns a uint64 with its bytes swapped
 func ByteSwapUInt64(value uint64) uint64 {
-	buf1 := make([]byte, 8)
-	buf2 := make([]byte, 8)
-	WriteUInt64(buf1, 0, value)
-	buf2[0] = buf1[7]
-	buf2[1] = buf1[6]
-	buf2[2] = buf1[5]
-	buf2[3] = buf1[4]
-	buf2[4] = buf1[3]
-	buf2[5] = buf1[2]
-	buf2[6] = buf1[1]
-	buf2[7] = buf1[0]
-	return ReadUInt64(buf2, 0)
+	return value>>56&0xff | value>>48&0xff<<8 | value>>40&0xff<<16 | value>>32&0xff<<24 |
+		value>>24&0xff<<32 | value>>16&0xff<<40 | value>>8&0xff<<48 | value&0xff<<56
 }
 
 // ByteSwapUInt32 returns a uint32 with its bytes swapped
 func ByteSwapUInt32(value uint32) uint32 {
-	buf1 := make([]byte, 4)
-	buf2 := make([]byte, 4)
-	WriteUInt32(buf1, 0, value)
-	buf2[0] = buf1[3]
-	buf2[1] = buf1[2]
-	buf2[2] = buf1[1]
-	buf2[3] = buf1[0]
-	return ReadUInt32(buf2, 0)
+	return value>>24&0xff | value>>16&0xff<<8 | value>>8&0xff<<16 | value&0xff<<24
 }

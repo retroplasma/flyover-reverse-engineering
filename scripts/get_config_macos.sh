63d4086502880c71520048b8892746ff7becac69
Andrian <retroplasma+gh@pm.me>
2020-04-11 23:21:53 +0200

    Update get_config_macos.sh

--- scripts/get_config_macos.sh
+++ scripts/get_config_macos.sh
@@ -2,8 +2,17 @@
 
 # Generates config.json from files on macOS
 
+set -Eeu
+
 token="$(strings /System/Library/PrivateFrameworks/GeoServices.framework/GeoServices | grep 'xyzABC' -A1 | tail -n1)"
 rmurl="$(plutil -p ~/Library/Preferences/com.apple.GEO.plist | grep GEOLastResourceManifestURL | awk '{print $3}' | cut -d '"' -f 2)"
+
+if [ -z "$rmurl" ]; then
+  tmp="$(mktemp -d)"
+  cp ~/Library/Containers/com.apple.geod/Data/Library/Caches/com.apple.geod/GEOConfigStore.db* "$tmp"
+  rmurl="$(sqlite3 "$tmp/GEOConfigStore.db" "select value from defaults where key = 'GEOLastResourceManifestURL';")"
+  rm -r "$tmp"
+fi
 
 echo "{
   \"resourceManifestURL\": \"$rmurl\",
